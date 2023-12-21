9026a13dfdd521dc0926590592a6e2492e5026e5
Daniel Mendes <daniel.mendes@reckon.ai>
2023-12-21 12:36:41 +0000

    feat: setup project

---
+++ cmd/release.sh
@@ -0,0 +1,25 @@
+release() {
+  local tag_prefix=$(echo "${1}" | sed 's/^@[^/]*\///')
+
+  yarn
+
+  # Bump version.
+  yarn version --no-git-tag-version --new-version ${2:-patch}
+
+  # Get the new version number.
+  local version=`grep -m1 version package.json | awk -F: '{ print $2 }' | sed 's/[", ]//g'`
+
+  # Add modified files.
+  git add .
+
+  # Get package name.
+  local packageName=$(tr '[:lower:]' '[:upper:]' <<< ${tag_prefix:0:tag_prefix})${tag_prefix:tag_prefix}
+
+  # Commit release with new version.
+  git commit -m "Release ${packageName} v${version}"
+
+  # Create tag.
+  git tag ${tag_prefix}/v${version}
+}
+
+release $1 $2
